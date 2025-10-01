a6d220d4bcc9b90bc0c0cdfb430e0b096db06451
lielie-peanut <laelie.huet@gmail.com>
2025-10-01 11:22:48 +0200

    added all equations except for catch and yield

--- R/recruitment_functions.R
+++ R/recruitment_functions.R
@@ -3,4 +3,44 @@ sex_change <- function(L,Li,deltaL, b){
   1/(1+exp(-b(L-(Li+deltaL))))
 }
 
-###
+###number of females
+
+N_F <- function(Nfemales,Pchange,Lfemales,Z){
+  (Nfemales-(Nfemales*Pchange(Lfemales)))*exp(-Z)
+}
+
+###number of males
+
+N_M <- function(Nfemales,Nmales,Pchange,Lfemales,Z){
+  (Nmales+(Nfemales*Pchange(Lfemales)))*exp(-Z)
+}
+
+###fecundity
+
+egg_count <- function(Beta0, Beta1, L){
+  exp(Beta0*L+Beta1)
+}
+
+###fertilization
+
+fert_rate <- function(k,xF){
+  (4*k*xF)/((1-k)+(5*k-1)*xF)
+}
+
+###male to ideal male comparison
+
+male_ratio <- function(Pt,Pz){
+  Pt/Pz
+}
+
+###stock recruitment
+
+spawn_recruitment <- function(h,R0, sigma,teta0){
+  (4*h*R0*sigma)/(R0*teta0*(1-h)+(5*h-1)*sigma)
+}
+
+###fishing recruitment
+
+fishing_recruitment <- function(L,Lmin, Lmax,mu,Fm){
+  (1/(1+exp(-((L-Lmin)/mu))))*(1-(1/(1+exp(-((L-Lmax)/mu)))))*Fm
+}
