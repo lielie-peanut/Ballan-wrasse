9ab79ea6ced1af06a937bdc79416291c30f7080b
lielie-peanut <laelie.huet@gmail.com>
2025-10-30 13:28:42 +0100

    test run of a function with my parameters

--- simulation.R
+++ simulation.R
@@ -7,5 +7,11 @@ source("R/recruitment_functions.R")
 
 #loading data
 parameters <- read.csv("data/parameters.csv", stringsAsFactors = F,sep = ";")
-params_list <- setNames(parameters$value,parameters$symbol)
+params_list <- as.list(setNames(parameters$value,parameters$symbol))
 print(params_list)
+
+#example run 
+ages <- 0:30
+Lf <- VB(ages,Linf = params_list$LinfF,k = params_list$KF,t0 = params_list$t0F)
+
+plot(ages, Lf, type = "l", xlab = "Age", ylab = "Length (mm)")
