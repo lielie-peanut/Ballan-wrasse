3bd9e437457a97f99cdb58fc4482cd3d2d1d8e34
lielie-peanut <laelie.huet@gmail.com>
2025-11-13 18:01:37 +0100

    added plots of the results. not conform to reported plots, check the year taken for plots in report.

--- results/results_production.R
+++ results/results_production.R
@@ -7,11 +7,12 @@ source("R/simulation.R")
 size_limits <- read.csv("data/size_limits.csv", stringsAsFactors = F,sep = ";")
 fishing_mortality_rates <- seq(from = 0.05, to = 1, by = 0.05)
 
-
+#creating arrays to store data
 size_array <- array(data = NA, dim = c(501, 20, length(size_limits$Lmin)),dimnames = list(years = c(0:500),mortality_rate = fishing_mortality_rates, size_lim = size_limits$location))
 catch_array <- array(data = NA, dim = c(501, 20, length(size_limits$Lmin)),dimnames = list(years = c(0:500),mortality_rate = fishing_mortality_rates, size_lim = size_limits$location))
 yield_array <- array(data = NA, dim = c(501, 20, length(size_limits$Lmin)),dimnames = list(years = c(0:500),mortality_rate = fishing_mortality_rates, size_lim = size_limits$location))
 
+#run the simulation and fill the arrays according to what was done
 for (i in 1:length(size_limits$Lmin)){
   count <- 1
   for (e in fishing_mortality_rates){
@@ -22,3 +23,47 @@ for (i in 1:length(size_limits$Lmin)){
     count <- count+1
   }
 }
+
+###plots###
+
+#population size by mortality rate under different size limits
+pop_size <- as.data.frame(
+  mortality_rate = fishing_mortality_rates,
+  size_array[501,,]
+)
+pop_size$mortality_rate <- as.numeric(row.names(pop_size))
+
+ggplot(data = pop_size, aes(x = mortality_rate)) +
+  geom_line(aes(y = `England Devon and Severn IFCA`, colour = "England Devon and Severn IFCA")) +
+  geom_line(aes(y = Norway, colour = "Norway")) +
+  geom_line(aes(y = Scotland, colour = "Scotland")) +
+  geom_line(aes(y = Sweden, colour = "Sweden")) +
+  geom_line(aes(y = theoretical, colour = "theoretical"))
+
+#catch by mortality rate
+catch_plot <- as.data.frame(
+  mortality_rate = fishing_mortality_rates,
+  catch_array[501,,]
+)
+catch_plot$mortality_rate <- as.numeric(row.names(pop_size))
+
+ggplot(data = catch_plot, aes(x = mortality_rate)) +
+  geom_line(aes(y = `England Devon and Severn IFCA`, colour = "England Devon and Severn IFCA")) +
+  geom_line(aes(y = Norway, colour = "Norway")) +
+  geom_line(aes(y = Scotland, colour = "Scotland")) +
+  geom_line(aes(y = Sweden, colour = "Sweden")) +
+  geom_line(aes(y = theoretical, colour = "theoretical"))
+
+#yield by mortality rate
+yield_plot <- as.data.frame(
+  mortality_rate = fishing_mortality_rates,
+  yield_array[501,,]
+)
+yield_plot$mortality_rate <- as.numeric(row.names(pop_size))
+
+ggplot(data = yield_plot, aes(x = mortality_rate)) +
+  geom_line(aes(y = `England Devon and Severn IFCA`, colour = "England Devon and Severn IFCA")) +
+  geom_line(aes(y = Norway, colour = "Norway")) +
+  geom_line(aes(y = Scotland, colour = "Scotland")) +
+  geom_line(aes(y = Sweden, colour = "Sweden")) +
+  geom_line(aes(y = theoretical, colour = "theoretical"))
